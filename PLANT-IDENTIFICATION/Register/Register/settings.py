880bee463211ff6fdc1f6133394ba233f084f0aa
DhruviShah16 <19dce123@charusat.edu.in>
2022-02-08 11:24:48 +0530

    mobile

--- PLANT-IDENTIFICATION/Register/Register/settings.py
+++ PLANT-IDENTIFICATION/Register/Register/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '71!^b&w_6cmk^0m2o7p2k#-3m^q0d_d!ygc(p)0tpq0h@75sbw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'Register.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':'DB1',
-        'USER':'postgres',
-        'PASSWORD':'NAND',
-        'HOST':'localhost',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- PLANT-IDENTIFICATION/Register/static/sketch.js
+++ PLANT-IDENTIFICATION/Register/static/sketch.js
@@ -1,33 +1,23 @@
-// Teachable Machine
-// The Coding Train / Daniel Shiffman
-// https://thecodingtrain.com/TeachableMachine/1-teachable-machine.html
-// https://editor.p5js.org/codingtrain/sketches/PoZXqbu4v
 
-// The video
 let video;
-// For displaying the label
 let label = "waiting...";
-// The classifier
 let classifier;
-let modelURL = 'https://teachablemachine.withgoogle.com/models/5ivg-nr0_/';
+let modelURL = 'https://teachablemachine.withgoogle.com/models/EOSNguWbZ/';
+//https://teachablemachine.withgoogle.com/models/8N-a_D9i3/';
 
-// STEP 1: Load the model!
+//https://teachablemachine.withgoogle.com/models/5ivg-nr0_/
 function preload() {
   classifier = ml5.imageClassifier(modelURL + 'model.json');
 }
 
-
 function setup() {
   var cnv = createCanvas(400, 400);
-cnv.position( windowWidth/2, windowHeight/6 );
-// Create the video
+   cnv.position( windowWidth/2 , windowHeight/6 );
   video = createCapture(VIDEO);
   video.hide();
-  // STEP 2: Start classifying
   classifyVideo();
 }
 
-// STEP 2 classify the videeo!
 function classifyVideo() {
   classifier.classify(video, gotResults);
 }
@@ -35,21 +25,26 @@ function classifyVideo() {
 function draw() {
   background(255);
 
-  // Draw the video
   image(video, 0,0,width, height);
 
 	
 	var ele = document.getElementById("output");
 	var ele2 = document.getElementById("description");
+    console.log(label)
+   if(label== "NOT FOUND")
+   {
+      ele.innerHTML = "NOT FOUND";
+      ele2.innerHTML = "Leaf is not visible";
+   }
 
-if(label == "Plumeria alba(champa)")
-	{
+    else if(label == "Plumeria alba(champa)")
+	 {
 	    ele.innerHTML ="Plumeria alba";
 		ele2.innerHTML = "Common Name: White Frangipani(champa) <br>\
                                   Habitate : Mexico, Central America, India <br>\
                                   Non-Poisonous <br>\
                                   Uses: Remedy for diarrhea, cure of itch, cough, asthama, fever, piles, blood disorder and tumor.";
-	}
+	 }
 	else if(label == "Terminalia catappa(Badam)"){
 		ele.innerHTML ="Terminalia catappa";
 		ele2.innerHTML = "Common Name: Almond(Badam) <br>\
@@ -86,19 +81,15 @@ if(label == "Plumeria alba(champa)")
 	}
 	//ele.appendChild(document.createTextNode(label))
 	
-  // Draw the emoji
-
 }
 
 
-// STEP 3: Get the classification!
+
 function gotResults(error, results) {
-  // Something went wrong!
   if (error) {
     console.error(error);
     return;
   }
-  // Store the label and classify again!
 		var button = document.getElementById("stop")
 if(button.checked){	
   	label = results[0].label;
